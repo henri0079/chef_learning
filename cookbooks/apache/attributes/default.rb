35a57266de080a943de23cefd0c5db30ada2ed15
root <root@workstation.(none)>
2016-10-17 17:11:14 +0000

    commit chef-repo for first timegit add .

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,12 @@
+default["apache"]["sites"]["henry-faily2"] = { "site_title" => "hooman website coming soo!" , "port" => 80, "domain" => "henry-faily2.mylabserver.com" }
+default["apache"]["sites"]["henry-faily2b"] = { "site_title" => "hooman 2 web comming soon" , "port" => 80, "domain" => "henry-faily2b.mylabserver.com" }
+default["apache"]["sites"]["henry-faily3"] = { "site_title" => "Hooman3  web site         " , "port" => 80, "domain" => "henry-faily3.mylabserver.com" }
+
+
+case node["platform"]
+when "centos"
+	default["apache"]["package"] = "httpd"
+when "ubuntu"
+	default["apache"]["package"] = "apache2"
+end
+
